889042212e6145bafb4b5f78bff21296d75e5523
Sarah Wilkes <aremnius@gmail.com>
2021-05-07 12:55:36 -0400

    begin adding custom palette, set bg color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#0F368F',
+        },
+        secondaryone: {
+          DEFAULT: '#301193',
+        },
+        secondarytwo: {
+          DEFAULT: '#008080',
+        },
+        complementary: {
+          DEFAULT: '#D68E00',
+        },
+        body: {
+          DEFAULT: 'rgba( 26, 6, 91,1)',
+        }
+      },
+    },
   },
   variants: {
     extend: {},
