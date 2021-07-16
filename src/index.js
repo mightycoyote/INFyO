4f5e446d0ae29ea62bdb60c8ccc5c3cb43796d0b
Sarah Wilkes <aremnius@gmail.com>
2021-07-16 14:20:10 -0400

    aside headers, fix link

--- src/index.js
+++ src/index.js
@@ -32,20 +32,22 @@ function App() {
                 </ul>
                 <p className="point">Most yoyos marketed to beginners and kids are still responsive yoyos. The slightly fancier ones can be converted to unresponsive.</p>
               </div>
-              <div className="box">
+              <aside className="box">
+                <span className="boxhead">The ol' switcheroo</span>
                 <p>Some yoyos are designed to work for either responsive or unresponsive play. Most of these can be switched back and forth by changing only the bearing and perhaps the axle, and often, both sets are included in the box (but read product descriptions to be sure).</p>
-                <p className="point">Occasionally, a yoyo can be set up either responsive or unresponsive, but it also requires different response pads for each. That's less trivial because response stickers or silicone can't be reused once they're removed (and can take some effort to do so since they don't come out cleanly). With these yoyos you'd decide which setup you want to use and mostly leave it.</p>
-              </div>
+                <p>Sometimes, a yoyo can be set up either responsive or unresponsive, but it also requires different response pads for each. That's less trivial because response stickers or silicone can't be reused once they're removed (and can take some effort to do so since they don't come out cleanly). With these yoyos you'd decide which setup you want to use and mostly leave it.</p>
+              </aside>
             </div>
             <h4>Playstyles</h4>
             <div className="content">
               <div className="primary">
                 <p>There are five main styles of play as reflected by the official <a href="https://en.wikipedia.org/wiki/World_Yo-Yo_Contest#Championship_Divisions">contest divisions</a>. You can also count an unofficial sixth&mdash;responsive string tricks, sometimes called "0A." If you're new to the hobby, check them out. Conversationally, the styles are often referred to by their division name.</p>
               </div>
-              <div className="box">
+              <aside className="box">
+                <span className="boxhead">The right tools</span>
                 <p>1A, 3A, and 5A all use string trick yoyos, mostly interchangeably. 2A and 4A use their own distinct yoyos specific to those playstyles.</p>
                 <p>The page you're on now is about getting started with basic 1A play.</p>
-              </div>
+              </aside>
             </div>
           <h3>Guts</h3>  
           <div className="content">
@@ -56,37 +58,39 @@ function App() {
                 <li><b>Ball bearing.</b> Ball bearings can be used for either responsive or unresponsive play, depending on the yoyo design, type of bearing, and whether the bearing spins freely or is lubed/greased to slow it down. The response system of most contemporary string trick yoyos consists of a 19mm diameter ring of silicone or silicone rubber. Silicone response lasts a fairly long time under normal use, but not forever. Some of the cheaper responsive bearing plastic yoyos use starburst response instead.</li>
               </ul> 
             </div>
-            <div className="box">
-              <p><b>Transaxle</b> yoyos are usually the best choice for younger kids and anyone who is not jazzed by the prospect of yoyo maintenance.</p>
-            </div>
+            <aside className="box">
+              <span className="boxhead">Keeping it simple</span>
+              <p>Transaxle yoyos are usually the best choice for younger kids and anyone who is not jazzed by the prospect of yoyo maintenance.</p>
+            </aside>
           </div>
           <h4>String trick yoyo bearings</h4>
           <div className="content">
             <div className="primary">
               <p>The majority of string trick yoyos all take the same standard diameter bearing. The narrow "half-spec" version is used for responsive play, and its wider cousin the "C" bearing for unresponsive play.</p>
               <ul>
-                <li><b>Half-spec bearings</b> need a thick lube or silicone grease in them to respond properly. When one comes installed in your new beginner-friendly yoyo, it's usually been lubed already, but eventually it will stop returning easily with a tug and you'll have to add more.</li> 
-                <li><b>"C" bearings</b> <a href="www.example.com">don't necessarily need lube</a>. But, they have more variations. Most yoyos come with one that's concave, or has a groove or channel in it, instead of a plain flat bearing. This helps keep the string towards the center of the bearing, which in turn makes it easier to keep the yoyo from drifting off plane. Centering bearings are popular for good reason, but bearing shape is also a matter of personal preference, and not all yoyos work equally well with all shapes.</li>
+                <li><b>Half-spec bearings</b> generally need a thick lube or silicone grease in them to respond properly. When one comes installed in your new beginner-friendly yoyo, it's usually been lubed already, but eventually it will stop returning easily with a tug and you'll have to add more.</li> 
+                <li><b>"C" bearings</b> <a href="#lube">can be played without lube</a>. But, they have more variations. Most yoyos come with one that's concave, or has a groove or channel in it, instead of a plain flat bearing. This helps keep the string towards the center of the bearing, which in turn makes it easier to keep the yoyo from drifting off plane. Centering bearings are popular for good reason, but bearing shape is also a matter of personal preference, and not all yoyos work equally well with all shapes.</li>
               </ul>
             </div>
-            <div className="box">
-              <p>Manufacturers do still make string trick yoyos that take bearings other than "C"&mdash;typically "A" or "D". That would be mentioned in the product description, though. If the size isn't mentioned and it's a contemporary string trick yoyo, you can assume it's size C. (If it's a collectors' item from the early days of ball-bearing yoyos, though, it could be anything.)</p>
-            </div>
+            <aside className="box">
+            <span className="boxhead">Big and small</span>
+              <p>Manufacturers do still make string trick yoyos that take bearings other than "C"&mdash;typically "D" or "A". That would be mentioned in the product description, though. If the size isn't mentioned and it's a contemporary string trick yoyo, you can assume it's size C. (If it's a collectors' item from the early days of ball-bearing yoyos, though, it might not be one of the current standard sizes at all.)</p>
+            </aside>
           </div>
-          <h3>Common materials</h3>
+          <h3>Materials</h3>
           <div className="columns">
             <div>
               <h4>Wood</h4>
-              <p>These are usually, but not always, fixed-axle. Decent cheap ones are still available, but you can also find fancier models with features like adjustable axles and interesting hardwoods.</p>
+              <p>These are usually, but not always, fixed-axle. Decent cheap ones are still available, but you can also find fancier models with features like adjustable axles and pretty hardwoods.</p>
             </div>
             <div>
               <h4>Plastic</h4>
-              <p>Plastic yoyos run the gamut from literally unusable dollar-store garbage to beautifully machined <a href="https://en.wikipedia.org/wiki/Polyoxymethylene">POM</a> (AKA "Delrin," which is a brand name) or polycarbonate that feel and play great and cost as much as a decent aluminum yoyo. On the other hand, you can also get a very good injection-molded yoyo that you'll use forever for $20 or less.</p>
+              <p>Plastic yoyos run the gamut from literally unusable dollar-store garbage to beautifully machined <a href="https://en.wikipedia.org/wiki/Polyoxymethylene">POM</a> (AKA "Delrin," which is a brand name) or <a href="https://en.wikipedia.org/wiki/Polycarbonate">polycarbonate</a> that feel and play great and cost as much as a decent aluminum yoyo. On the other hand, you can also get a very good injection-molded yoyo that you'll enjoy forever for $20 or less.</p>
             </div>
             <div>
               <h4>Aluminum</h4>
               <ul>
-                <li><b>6061</b>. Most aluminum yoyos are made out of a 6061 alloy. It's the standard, and you can assume an aluminum yoyo is made out of 6061 unless the listing states otherwise, or, it's dirt-cheap and of dubious origin.</li>
+                <li><b>6061</b>. Most aluminum yoyos are made out of a 6061 alloy. It's the standard, and you can assume an aluminum yoyo is made out of 6061 unless the listing states otherwise, or, it's of dubious origin.</li>
                 <li><b>7000 series</b>: usually 7068 or 7075. Some higher-end aluminum yoyos are made out of these. Harder and denser than 6061, they can be machined a little thinner, and can result in higher-performing, slightly more damage-resistant yoyos. They hit your hand (or any other body parts that get in the way) harder accordingly, though.</li>
               </ul>
               <p className="point breakafter"><a href="https://en.wikipedia.org/wiki/Aluminium_alloy#Wrought_alloys">If you're interested, you can read all about aluminum alloys in Wikipedia.</a></p>
@@ -94,7 +98,7 @@ function App() {
             <div>
               <h4>Titanium</h4>
               <p>Usually unresponsive, although there are also some responsive slimline titanium yoyos. The advantage of titanium is similar to 7000-series aluminum, only even more so&mdash;it can be machined even thinner, and is very durable. However, it does not perform objectively better than aluminum. (If that were the case, titanium would routinely be used by serious competitors, but it isn't.) They're pricey, and often very limited, and you're not really missing out if you don't have one.</p>
-              <p className="point">... That said, if you want a Ti anyway, one good way to go about it is to keep an eye out for preorder announcements. They're sometimes available used too, of course, but not necessarily much cheaper.</p>
+              <p className="point">... That said, if you want titanium anyway, one good way to go about it is to keep an eye out for preorder announcements. They're sometimes available used too, of course, but not necessarily at a bargain price.</p>
             </div>
             <div>
               <h4>Steel</h4>
@@ -124,9 +128,10 @@ function App() {
                   </li>
                 </ul>
               </div>
-            <div className="box">
+            <aside className="box">
+            <span className="boxhead">Thick and thin</span>
               <p>Strings also come in different thicknesses. There are some variations between the different manufacturers, and many of them also offer different thicknesses of their main formula. Thickness is just another personal preference. What you like sometimes depends on the specific yoyo, too.</p>
-            </div>
+            </aside>
             </div>
         </section>
         <section>
@@ -138,7 +143,7 @@ function App() {
               <li>Just get two yoyos. One of them can be a cheap responsive and the other can be a nicer one that can be played unresponsive. Or you could get two different switchable ones, and see what setup you prefer on each, etc. This would also let you try out different shape profiles.</li>
             </ol>
             {/* putting styling inside a point class element breaks it, not sure why yet */}
-            <p className="point">What I <em>don't</em> recommend is trying to learn the basics on a responsive aluminum yoyo. Some people do, but... ouch. Start out with a plastic one!</p>
+            <p className="point">What I <em>don't</em> recommend is trying to learn the basics on a responsive aluminum yoyo. Some people do, but... ouch.</p>
             <h3>Options</h3>
               <div className="content">
                 <div className="primary">
@@ -148,10 +153,11 @@ function App() {
                     <li>Look on yoyo forums to see what people are recommending currently, or ask. The tricky part is that not everything is always available, so you can get help choosing from your <em>current</em> options too. People's favorite recommendations aren't always in stock.</li>
                   </ol>
                 </div>
-                <div className="box">
+                <aside className="box">
+                <span className="boxhead">Being prepared</span>
                   <p>The one thing you should definitely buy with your first yoyo is extra string (if you're not getting a package deal that comes with it already). They don't need to be fancy or expensive, and a 100-pack might be overkill, but you do need some.</p>
                   <p className="point">Many people find dark string too hard to see. Bright yellow, green, orange, or pink are recommended instead. If you really want to get something like blue or purple, just buy a bright color too so that you have an alternative.</p>
-                </div>
+                </aside>
               </div>
             <h3>Resources</h3>
               <h4>Videos</h4>
@@ -162,18 +168,18 @@ function App() {
         <section>
           <h2>FAQs with somewhat controversial answers</h2>
             <div>
-              <h3>Unresponsive lube</h3>
+              <h3 id="lube">Unresponsive lube</h3>
               <ul>
               <p><b>Should I use thin lube on my unresponsive bearings?</b></p>
               <li>
-                <p><b>Arguments in favor</b>: Reduced bearing noise, rust protection, theoretical increase in longevity.</p>
-                <p className="point">Note that people living in some very humid and/or salty climates report that bearing lube isn't really optional for them.</p>
+                <p><b>Arguments in favor</b>: Reduced bearing noise, rust protection, theoretical increase in longevity and smoothness.</p>
+                <p className="point">Note that people living in some very humid and/or salty climates report that bearing lube isn't really optional for them. The bearings really do rust otherwise.</p>
               </li>
               <li>
                 <p><b>Arguments against</b>: Makes bearing slower or responsive, attracts crud, does not actually improve longevity, is a waste of time and money.</p>
               </li>
               <li>
-                <p><b>My opinion</b>: Feel free to use thin lube, for obnoxiously noisy bearings at the very least. Use a pin or similar to apply a very small amount, ideally less than a drop. I've seen some instructions telling people to put in way too much. Too much does make the bearing semi-responsive, and then you'd need to clean it and start over.</p>
+                <p><b>My opinion</b>: Feel free to use thin lube, for noisy bearings at the very least. Use a pin or similar to apply a very small amount, ideally less than a drop. I've seen some instructions telling people to put in way too much. Too much does make the bearing semi-responsive, and then you'd need to clean it and start over.</p>
               </li>
               </ul>
             </div>
@@ -190,13 +196,15 @@ function App() {
                       <p><b>Argument against</b>: Spending some time with a responsive yoyo first makes it easier to learn the basics and makes your technique cleaner. And, it can help keep you from getting into bad habits that are hard to break later.</p>
                     </li>
                     <li>
-                      <p><b>My opinion</b>: You should play with a responsive yoyo at least until you can throw straight, and then a few basic tricks until you get a feel for how you have to play to keep the yoyo from snapping back prematurely. And, from a purely practical standpoint, learning the very basic basics on a unresponsive yoyo would be pretty tedious&mdash;lots of winding the yoyo back up by hand, not so much practicing tricks.</p>
+                      <p><b>My opinion</b>: You should play with a responsive yoyo at least until you can throw straight (frontstyle and breakaway), and then a few basic tricks until you get a feel for how you have to play to keep the yoyo from snapping back prematurely.</p>
+                      <p>From a purely practical standpoint, learning the most basic basics on a unresponsive yoyo would be pretty tedious&mdash;lots of winding the yoyo back up by hand, not so much doing tricks.</p>
                     </li>
                   </ul>
                 </div>
-                <div className="box">
+                <aside className="box">
+                  <span className="boxhead">Not a destination</span>
                   <p>A lot of people seem to overthink this "when can I graduate to unresponsive" question. Most players I know of continue to enjoy responsive yoyos forever&mdash;It's not really an either/or thing. That's one of the reasons it's good to have one of each available when you're ready to start messing around with unresponsive play.</p>
-                </div>
+                </aside>
               </div>
 
             </div>
@@ -218,7 +226,7 @@ function App() {
               <p>Yeah, they come in a lot of shapes.</p>
               <ul>
                 <li>You'll often see shapes broadly categorized as "V," "W," "H," or "O," which is just a shorthand since many designs don't fit neatly into one of these, and, it doesn't always tell you much. (The shape-naming convention is explained in detail elsewhere online, with diagrams, by people more knowledgeable than me, so I'm not repeating it here.)</li>
-                <li>A typical contemporary performance-oriented (or competition/"comp") yoyo has one of the more angular profiles (as opposed to fully rounded), a lot of rim weight, and is often fairly wide. Beyond those, there's an enormous variety of yoyos that are popular for their fun factor, feel in the hand, and suitability for more casual playstyles, instead of pure performance.</li>
+                <li>A typical contemporary performance-oriented (or competition/"comp") yoyo has one of the more angular profiles (as opposed to fully rounded), a lot of rim weight, and is often fairly wide. Beyond them, there's an enormous variety of yoyos that are popular for their fun factor, feel in the hand, and suitability for more casual playstyles, instead of pure performance.</li>
                 <li>When you're new, it's easy to get the idea that you'll always want to play with performance-oriented yoyos. That's true to an extent when you're starting out or working on something new&mdash;it's easier to land and figure out the tricks you're learning. Just don't be surprised if your taste in yoyos evolves over time, or even day to day, after you've been playing for a while.</li>
                 <li>The specific shape you get for your first yoyo doesn't matter much. If you get into it, you'll end up trying a lot of them to see which you like best.</li>
               </ul>
