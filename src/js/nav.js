b0cda761197eb2e27800908bf9c902b402bae876
Sarah Wilkes <aremnius@gmail.com>
2021-09-08 09:38:32 -0400

    fix uneven margin in nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,13 +1,13 @@
 function Nav() {
     return (
         <nav className="py-4">
-            <ul className="clear-both flex flex-col md:flex-row list-none place-items-center justify-center text-secondaryone-verydark m-8 uppercase tracking-wider font-light text-sm no-underline border-t border-b border-secondaryone-verydark">
-                <li><a href="#basics">Basics</a></li>
-                <li><a href="#guts">Guts</a></li>
-                <li><a href="#materials">Materials</a></li>
-                <li><a href="#strings">Strings</a></li>
-                <li><a href="#gettingstarted">Getting started</a></li>
-                <li><a href="#faqs">FAQs</a></li>
+            <ul className="clear-both flex flex-col md:flex-row list-none place-items-center justify-center text-secondaryone-verydark m-8 p-1 uppercase tracking-wider font-light text-sm no-underline border-t border-b border-secondaryone-verydark">
+                <li className="p-0"><a href="#basics">Basics</a></li>
+                <li className="p-0"><a href="#guts">Guts</a></li>
+                <li className="p-0"><a href="#materials">Materials</a></li>
+                <li className="p-0"><a href="#strings">Strings</a></li>
+                <li className="p-0"><a href="#gettingstarted">Getting started</a></li>
+                <li className="p-0"><a href="#faqs">FAQs</a></li>
             </ul>
         </nav>
     );
